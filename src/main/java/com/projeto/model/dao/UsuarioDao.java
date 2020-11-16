02ff16c2a82ac0f1c4a3f464f75a04b419e1803d
Vinicius Henrique <viniciushenrique5419@hotmail.com>
2020-11-16 11:57:55 -0300

    Create UsuarioDao.java

---
+++ src/main/java/com/projeto/model/dao/UsuarioDao.java
@@ -0,0 +1,12 @@
+package com.projeto.model.dao;
+
+import javax.persistence.EntityManager;
+import com.projeto.model.models.Usuario;
+
+public class UsuarioDao extends GenericDao<Usuario, Integer>{
+
+	public UsuarioDao(EntityManager entityManager) {
+		super(entityManager);
+	}
+
+}
