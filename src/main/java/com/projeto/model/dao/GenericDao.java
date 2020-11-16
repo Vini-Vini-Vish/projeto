e6ab3e0b374ff1ccd1af3a2c9fc8b781adad8573
Vinicius Henrique <viniciushenrique5419@hotmail.com>
2020-11-16 12:53:03 -0300

    Update GenericDao.java

--- src/main/java/com/projeto/model/dao/GenericDao.java
+++ src/main/java/com/projeto/model/dao/GenericDao.java
@@ -2,7 +2,11 @@ package com.projeto.model.dao;
 
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 public abstract class GenericDao<T, Id extends Serializable>{
 	
@@ -32,8 +36,22 @@ public abstract class GenericDao<T, Id extends Serializable>{
 		return this.getEntityManager().find(getClassePersistencia(), id);
 	}
 	
+	@SuppressWarnings("unchecked")
+	 public List<T> findAll(Class<T> classe){
+		 List<T> lista = new ArrayList<>();
+		 Query query = this.getEntityManager().createQuery("SELECT o FROM "+classe.getSimpleName()+" o");
+		 
+		 lista = query.getResultList();
+		 
+		 return lista;		 
+	 }
+	
 	public EntityManager getEntityManager() {
 		return entityManager;
+	}
+	
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
 	}
 
 	public Class<T> getClassePersistencia() {
